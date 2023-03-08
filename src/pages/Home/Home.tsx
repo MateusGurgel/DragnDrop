9e031a00009d6e5b0b4eb7fd09ce8a7a80db943b
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-03-08 20:06:01 -0300

    initial commit

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,50 @@
+import "./Home.css"
+
+import { Droppable } from "react-beautiful-dnd";
+import Card from "../../components/Card";
+
+const bestTools = [
+  {
+    id: "Discord",
+    name: "Discord",
+    picture: "later",
+  },
+  {
+    id: "VScode",
+    name: "Visual Studio Code",
+    picture: "later",
+  },
+  {
+    id: "Firefox",
+    name: "Firefox",
+    picture: "later",
+  },
+];
+
+const Home = () => {
+  return (
+    <div id="home">
+    <Droppable droppableId="droppable">
+      {(provided) => (
+        <ul
+          ref={provided.innerRef}
+          {...provided.droppableProps}
+          {...provided.innerRef}
+        >
+          {bestTools.map((tool, index) => (
+            <Card
+              key={tool.id}
+              id={tool.id}
+              name={tool.name}
+              picture={tool.picture}
+              index={index}
+            ></Card>
+          ))}
+        </ul>
+      )}
+    </Droppable>
+    </div>
+  );
+};
+
+export default Home;
